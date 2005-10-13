04373c0c448d5f58af1cc34cdb79bc920d19e3af
woobster <>
2005-10-13 16:37:18 +0000

    2005-10-13  Bob Doan  <bdoan@sicompos.com> 	* libsrc/breaks.c: Don't layout when precalculating

--- libsrc/breaks.c
+++ libsrc/breaks.c
@@ -199,9 +199,11 @@ static void rlib_break_all_below_in_reverse_order(rlib *r, struct rlib_part *par
 			bf->rval = NULL;
 		}
 		if(OUTPUT(r)->do_break) {
-			gint did_end_page;
+			gint did_end_page = FALSE;
 			
-			did_end_page = rlib_end_page_if_line_wont_fit(r, part, report, rb->footer);
+			
+			if(precalculate == FALSE)
+				did_end_page = rlib_end_page_if_line_wont_fit(r, part, report, rb->footer);
 
 			if(!INPUT(r, r->current_result)->isdone(INPUT(r, r->current_result), r->results[r->current_result].result))
 				rlib_navigate_previous(r, r->current_result);
