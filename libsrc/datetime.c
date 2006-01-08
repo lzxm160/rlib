90009c81a15909bdee60ab47c61f758009563a8b
woobster <>
2006-01-08 23:19:08 +0000

    2006-01-08  Bob Doan  <bdoan@sicompos.com> 	* Makefile.am: New year, new copyright 	* bindings/Makefile.am: 	* bindings/csharp/Makefile.am: 	* bindings/interfaces/Makefile.am: 	* bindings/java/Makefile.am: 	* bindings/perl/Makefile.am: 	* bindings/python/Makefile.am: 	* configure.in: 	* doc/Makefile.am: 	* inputs/Makefile.am: 	* inputs/mysql/Makefile.am: 	* inputs/mysql/mysql.c: 	* inputs/odbc/Makefile.am: 	* inputs/postgres/Makefile.am: 	* inputs/postgres/postgres.c: 	* libsrc/Makefile.am: 	* libsrc/api.c: 	* libsrc/axis.c: 	* libsrc/breaks.c: 	* libsrc/charencoder.c: 	* libsrc/charencoder.h: 	* libsrc/csv.c: 	* libsrc/datasource.c: 	* libsrc/datetime.c: 	* libsrc/datetime.h: 	* libsrc/environment.c: 	* libsrc/formatstring.c: 	* libsrc/free.c: 	* libsrc/fxp.c: 	* libsrc/gd.c: 	* libsrc/graphing.c: 	* libsrc/html.c: 	* libsrc/layout.c: 	* libsrc/load.c: 	* libsrc/navigation.c: 	* libsrc/parsexml.c: 	* libsrc/pcode.c: 	* libsrc/pcode.h: 	* libsrc/pcode_op_functions.c: 	* libsrc/pdf.c: 	* libsrc/ralloc.c: 	* libsrc/ralloc.h: 	* libsrc/reportgen.c: 	* libsrc/resolution.c: 	* libsrc/rlib.h: 	* libsrc/rlib_gd.h: 	* libsrc/rlib_input.h: 	* libsrc/save.c: 	* libsrc/txt.c: 	* libsrc/util.c: 	* libsrc/util.h: 	* libsrc/value.c: 	* libsrc/value.h: 	* libsrc/variables.c: 	* libsrc/xml_data_source.c: 	* rpdf/Makefile.am: 	* rpdf/rpdf.c: 	* rpdf/rpdf.h: 	* rpdf/test.c: 	* src/Makefile.am: 	* src/examples/Makefile.am: 	* src/examples/c/Makefile.am: 	* src/examples/c/example.c: 	* src/examples/common/Makefile.am: 	* src/examples/csharp/Makefile.am: 	* src/examples/java/Makefile.am: 	* src/examples/perl/Makefile.am: 	* src/examples/php/Makefile.am: 	* src/examples/python/Makefile.am: 	* src/rlibc.c: 	* tests/Makefile.am: 	* tests/test1.c: 	* tests/test2.c: 	* tests/test3.c:

--- libsrc/datetime.c
+++ libsrc/datetime.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003-2005 SICOM Systems, INC.
+ *  Copyright (C) 2003-2006 SICOM Systems, INC.
  *
  *  Authors: Chet Heilman <cheilman@sicompos.com>
  *
--- libsrc/xml_data_source.c
+++ libsrc/xml_data_source.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003-2005 SICOM Systems, INC.
+ *  Copyright (C) 2003-2006 SICOM Systems, INC.
  * 
  *  Authors: Jeremy Lee <jlee@platinumtel.com> 
  *           Warren Smith <wsmith@platinumtel.com>
