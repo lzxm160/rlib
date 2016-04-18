0d3de1edd30617fcd536a880c6a592f7050a091a
Böszörményi Zoltán <zboszormenyi@sicom.com>
2016-04-18 09:44:32 +0200

    RLIB-17: Add a new C unit test
    Implemented the previously added PHP unittest called
    "flow_part_subdir.php" with the quirk that it doesn't have any
    data sources but still uses the <load> tag and references
    fields from data sources.

---
+++ src/examples/c/relprt.c
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (C) 2016 SICOM Systems, INC.
+ *
+ *  Author: Zoltán Böszörményi <zboszormenyi@sicom.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+#include <rlib.h>
+
+int main(int argc, char **argv) {
+	rlib *r;
+
+	r = rlib_init();
+	rlib_add_search_path(r, "parts/subdir2");
+	rlib_add_report(r, "parts/flow_part_subdir.xml");
+	rlib_set_output_format(r, RLIB_FORMAT_PDF);
+	rlib_execute(r);
+	rlib_spool(r);
+	rlib_free(r);
+	return 0;
+}
